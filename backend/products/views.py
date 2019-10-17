456c80c404a2a78aff7b424e2cba9bd5a6271968
rajvijay <raju.rajvijay619@gmail.com>
2019-10-17 08:18:58 +0530

    create apps for delivery,cart,customers

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,17 +5,17 @@ from .models import Product
 # Create your views here.
 def product_list(request):
     # pass
-    # products = Product.objects.all()
-    # json_response = []
-    # for product in products:
-    #     json_response.append({
-    #         "product_id" : product.product_id,
-    #         "product_title":product.product_title,
-    #         "product_price":product.product_price,
-    #         "product_description":product.product_description,
-    #         "product_category":product.product_category
-    #     })
-    # return JsonResponse(json_response,safe=False)
+    products = Product.objects.all()
+    json_response = []
+    for product in products:
+        json_response.append({
+            "product_id" : product.product_id,
+            "product_title":product.product_title,
+            "product_price":product.product_price,
+            "product_description":product.product_description,
+            "product_category":product.product_category
+        })
+    return JsonResponse(json_response,safe=False)
     pass
 
 def product_detail(request):
